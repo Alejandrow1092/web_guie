0ccf32707782539f86151e53cd45d286d001b85f
Ernesto Cedillo <rafacrteto@outlook.com>
2021-02-11 05:24:18 -0600

    Primer avance header

--- public/es6/index.js
+++ public/es6/index.js
@@ -1,1 +1,17 @@
-alert("hola mundo")
+/*addEventListener('DOMContentLoaded', () =>{
+    const icono_menu = document.querySelector('#icono-menu')
+    if (icono_menu){
+        icono_menu.addEventListener('click',() => {
+            const nav = document.querySelector('.conten_menu')
+            nav.classList.toggle('.active')
+        })
+    }
+})*/
+
+
+function accion(){
+    let d1;
+    d1=document.getElementById("1").style.left="0vw";
+
+}
+
